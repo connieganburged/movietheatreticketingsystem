5f700e6f431db7f2a761f60ae13b27fde2f8f49d
connieganburged <connie.g@asu.edu.mn>
2022-03-21 13:57:25 +0800

    1

--- primecineplex.py
+++ primecineplex.py
@@ -1,3 +1,4 @@
-"""
-My Program
-"""
+print ("Hello, welcome to Prime Cineplex. How can I help you today?")
+info= input('Would you like to connect to a live admin? (yes/no)')
+if info == 'yes':
+    print()
