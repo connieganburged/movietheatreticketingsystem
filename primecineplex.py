6aebe65a1b38cc7b29d970544062312df788479c
connieganburged <98507970+connieganburged@users.noreply.github.com>
2022-03-23 12:57:00 +0800

    Update primecineplex.py

--- primecineplex.py
+++ primecineplex.py
@@ -1,4 +1,23 @@
 print ("Hello, welcome to Prime Cineplex. How can I help you today?")
 info= input('Would you like to connect to a live admin? (yes/no)')
 if info == 'yes':
-    print()
+    print('An admin will contact you shortly, please wait. Thank you.')
+if info == 'no':
+    print('1. Location and Work Hours, 2. Phone Number and Contact, 3. Prices 4. Book a Movie')
+
+choice = input('')
+if choice == '1':
+    print('We are located in Encanto Tower and Zaisan Hill from 10:00-0:00')
+if choice == '2':
+    print('Call us at +976 7510 0000. Our website is primecineplex.mn')
+if choice == '3':
+    print('a. General Admission, b. Dolby Atmos, c. VIP')
+    type = input: ('a b or c')
+        if type == 'a':
+            print('ADULT 2D: 9,00 3D: 10,000 CHILD 2D: 6,000 3D: 7,000')
+        if type == 'b':
+            print('ADULT 2D: 6,500 3D: 11,000 CHILD 2D: 6,500 3D: 7,000')
+        if type == 'c':
+            print(20000
+if choice == '4':
+    print
