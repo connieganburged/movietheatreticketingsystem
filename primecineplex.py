5b703beead85f2fac7604cd283a187638e139439
connieganburged <98507970+connieganburged@users.noreply.github.com>
2022-04-07 09:43:49 +0800

    Update primecineplex.py

--- primecineplex.py
+++ primecineplex.py
@@ -4,28 +4,34 @@ By Connie
 """
 from tkinter import N
 
-#Seats 
-seats = (([3,4], [ ], [ ]), ([ ], [ ], [ ]), ([ ], [ ], [ ]))
+#If rebook is chosen, the program will restart
+rebook = 'yes'
+while rebook == 'yes':  
+   #Seats 
+   seats = (([3,4], [ ], [ ]), ([ ], [ ], [ ]), ([ ], [ ], [ ]))
 
+   def func1():
+      print("Being processed, please wait.")
    
-info = None
-while info not in ('yes','no'):
-    info= input('Would you like to connect to a live admin? (yes/no): ')
-if info == 'yes':
-    print('An admin will contact you shortly, please wait. Thank you.')
-if info == 'no':
-    choice = input('1. Location and Work Hours, 2. Phone Number and Contact, 3. Prices 4. Book a Movie: ')
+   info = None
+   while info not in ('yes','no'):
+      info= input('Would you like to connect to a live admin? (yes/no): ')
+   if info == 'yes':
+      print('An admin will contact you shortly, please wait. Thank you.')
+   if info == 'no':
+      choice = input('1. Location and Work Hours, 2. Phone Number and Contact, 3. Prices 4. Book a Movie: ')
 
-#Provide the service they requested
-match choice:
-    case '1':
+   #Provide the service they requested
+      func1()
+      match choice:
+      case '1':
         print('We are located in Encanto Tower and Zaisan Hill from 10:00-0:00')
-    case '2':
+      case '2':
         print('Call us at +976 7510 0000. Our website is primecineplex.mn')
-    case '3':
+      case '3':
         print('a. General Admission, b. Dolby Atmos, c. VIP')
-        choice = input ('a b or c: ')
-        match choice:
+         choice = input ('a b or c: ')
+         match choice:
             case 'a':
                 print('ADULT 2D: 9,00 3D: 10,000 CHILD 2D: 6,000 3D: 7,000')
             case 'b':
@@ -34,7 +40,7 @@ match choice:
                 print('20,000')
             case 'd':
                 print('2D: 15,000 3D: 16,000')
-    case '4':
+      case '4':
         #Display movies
         print ('Here are the movie choices. 1. Barbie 2. Zootopia 3. Annabelle')
         #validate movie choice
@@ -72,8 +78,18 @@ match choice:
             if chosen_seat not in range(1,21): 
                 print("Seat not available, choose a different seat")
                 chosen_seat = None
+               
+   #print order information
+   print(f"Movie: {movie_choice}, Time: {session_choice}")
 
-#Ask if they want to rebook
+   rebook = None
+   #Ask if they want to rebook
+   while rebook not in ('yes','no'):
+      rebook = (input("Would you like to rebook your order? "))
+      if rebook == 'yes':
+         print('Rebooking is starting: ')
+         continue
+      elif rebook == 'no':
+         print("Thank you for choosing Prime Cineplex!")
 
-
-print('Thank you. Have a nice day!')
+   print('Have a nice day!')
